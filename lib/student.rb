1920268f8c79a33b7d165a6901d3b57b9a94ccf7
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-04 01:56:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,8 +13,13 @@ class Student
   def self.create_table
     sql = <<-SQL
         CREATE TABLE students(
-
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          grade INTEGER
         );
     SQL
+
+    DB[:conn].execute(sql)
+  end
 
 end
